562a3a9b943fa6a74248bae91f3f58695a72cdcb
JoEunsoo <dcpg95@gmail.com>
2022-12-14 23:35:42 +0900

    CHORE : 초기세팅

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,24 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import Helmet from 'helmet';
+import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3005);
+  app.use(Helmet());
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'HEAD', 'PUT', 'POST', 'DELETE'],
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  await app.listen(process.env.PORT || 3006);
 }
 bootstrap();
