1db6c1a03e83c4e60d2c38e52da93986c134023e
JoEunsoo <dcpg95@gmail.com>
2022-12-14 23:38:43 +0900

    FEAT : auth 모듈 설정

---
+++ api/src/modules/auth.module.ts
@@ -0,0 +1,13 @@
+import { UsersEntity } from 'src/entities/users.entity';
+import { UsersRepository } from '../repositories/users.repository';
+import { Module } from '@nestjs/common';
+import { AuthController } from 'src/controllers/auth.controller';
+import { AuthService } from 'src/services/auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UsersEntity])],
+  providers: [AuthService, UsersRepository],
+  controllers: [AuthController],
+})
+export class AuthModule {}
