9ed487f750ffe7669c0cd0ec604a2593b38b4218
JoEunsoo <dcpg95@gmail.com>
2022-12-15 23:34:08 +0900

    CHORE : posts 모듈 추가

--- api/src/modules/app.module.ts
+++ api/src/modules/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ormConfig } from '../config/ormconfig';
 import { AuthModule } from './auth.module';
+import { PostModule } from './post.module';
 
 @Module({
   imports: [
     AuthModule,
     TypeOrmModule.forRoot(ormConfig),
     ConfigModule.forRoot({ isGlobal: true }),
+    PostModule,
   ],
   controllers: [],
   providers: [],
