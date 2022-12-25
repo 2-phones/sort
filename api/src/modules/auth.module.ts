deeb3213e3c9daf463f13e7a280cd89d8847721a
JoEunsoo <dcpg95@gmail.com>
2022-12-25 23:24:46 +0900

    FEAT : 모듈 추가

--- api/src/modules/auth.module.ts
+++ api/src/modules/auth.module.ts
@@ -4,9 +4,18 @@ import { Module } from '@nestjs/common';
 import { AuthController } from 'src/controllers/auth.controller';
 import { AuthService } from 'src/services/auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsersEntity])],
+  imports: [
+    TypeOrmModule.forFeature([UsersEntity]),
+    JwtModule.register({
+      secret: process.env.SECRETKEY,
+      signOptions: {
+        expiresIn: '15m',
+      },
+    }),
+  ],
   providers: [AuthService, UsersRepository],
   controllers: [AuthController],
 })
