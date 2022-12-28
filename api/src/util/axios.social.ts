14a8fbb054895833818cfee1e392fc008947690f
JoEunsoo <dcpg95@gmail.com>
2022-12-28 14:23:30 +0900

    CHORE : 불필요한 코드 제거

--- api/src/util/axios.social.ts
+++ api/src/util/axios.social.ts
@@ -1,9 +1,9 @@
-import { LoginDto } from 'src/dto/auth/login.dto';
+import { SocialDto } from 'src/dto/auth/login.dto';
 import axios from 'axios';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-export const getSocialData = async (data: LoginDto) => {
+export const getSocialData = async (data: SocialDto) => {
   const { access_token } = data;
 
   return data.social_type === 'naver'
@@ -26,12 +26,18 @@ const naverAPI = async (access_token: string) => {
 
 const kakaoAPI = async (access_token: string) => {
   try {
-    const result = await axios.get(process.env.KAKAOURL, {
+    const getkakao = await axios.get(process.env.KAKAOURL, {
       headers: {
         Authorization: `Bearer ${access_token}`,
       },
     });
-    return result.data;
+    const { id: social_id } = getkakao.data;
+    const { nickname: user_name, profile_image: profile_url } =
+      getkakao.data.properties;
+    const { email } = getkakao.data.kakao_account;
+    const result = { social_id, user_name, profile_url, email };
+
+    return result;
   } catch (err) {
     return err;
   }
