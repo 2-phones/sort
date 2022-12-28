ef7a7d8200816abcc725e7061af0466c2d811f84
JoEunsoo <dcpg95@gmail.com>
2022-12-28 14:24:24 +0900

    FEAT : 회원 정보 로직 구현

--- api/src/services/user.service.ts
+++ api/src/services/user.service.ts
@@ -1,10 +1,16 @@
+import { UsersRepository } from './../repositories/users.repository';
 import { Injectable } from '@nestjs/common';
 import { TokenService } from './token.service';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly tokenService: TokenService) {}
-  async getUser() {
-    return 'ds';
+  constructor(
+    private readonly userRepository: UsersRepository,
+    private readonly tokenService: TokenService,
+  ) {}
+  async getUser(token: string) {
+    const { user_id } = await this.tokenService.decodeToken(token);
+    const result = await this.userRepository.selectProfile(user_id);
+    return result;
   }
 }
