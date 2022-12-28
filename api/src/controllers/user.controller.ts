a1d9055f41cde48e358c05e66f0e3139af792933
JoEunsoo <dcpg95@gmail.com>
2022-12-28 14:21:59 +0900

    FEAT : 회원 컨트롤러 구현

--- api/src/controllers/user.controller.ts
+++ api/src/controllers/user.controller.ts
@@ -1,10 +1,13 @@
+import { UserService } from './../services/user.service';
 import { Controller, Get, Headers } from '@nestjs/common';
 
-@Controller('user')
+@Controller('sort/user')
 export class UserController {
-  @Get()
-  getProfile(@Headers() headers: any) {
+  constructor(private userService: UserService) {}
+  @Get('/profile')
+  async getProfile(@Headers() headers: any) {
     const token = headers['authorization'].split(' ')[1];
-    return token;
+
+    return await this.userService.getUser(token);
   }
 }
