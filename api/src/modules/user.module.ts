f2499680830107175e3926ac509d68194176db90
JoEunsoo <dcpg95@gmail.com>
2022-12-28 14:22:28 +0900

    FEAT : user module 세팅

--- api/src/modules/user.module.ts
+++ api/src/modules/user.module.ts
@@ -1,11 +1,14 @@
+import { UsersRepository } from './../repositories/users.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TokenModule } from './token.module';
 import { UserController } from './../controllers/user.controller';
 import { Module } from '@nestjs/common';
 import { UserService } from 'src/services/user.service';
+import { UsersEntity } from 'src/entities/users.entity';
 
 @Module({
-  imports: [TokenModule],
-  providers: [UserService],
+  imports: [TypeOrmModule.forFeature([UsersEntity]), TokenModule],
+  providers: [UserService, UsersRepository],
   controllers: [UserController],
 })
 export class UserModule {}
