a7907c23a270cef586417dbe112ce285bab01938
JoEunsoo <dcpg95@gmail.com>
2023-01-08 01:36:04 +0900

    FEAT : redux custom hook

--- client/src/hooks/redux.hook.ts
+++ client/src/hooks/redux.hook.ts
@@ -2,4 +2,4 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, DispatchType } from '../redux/store';
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const useAppDispatch = useDispatch<DispatchType>();
+export const useAppDispatch = () => useDispatch<DispatchType>();
