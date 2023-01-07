2c0431486d03de640694170ca06051dd6e60b6b4
JoEunsoo <dcpg95@gmail.com>
2023-01-08 00:16:22 +0900

    CHORE : 컨트롤러 response 추가

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -1,7 +1,9 @@
+import { SignupDto } from 'src/dto/auth/signup.dto';
 import { TokenService } from 'src/services/token.service';
 import { AuthService } from './../services/auth.service';
-import { Body, Controller, Get, Headers, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
 import { LoginDto } from 'src/dto/auth/login.dto';
+import { Response } from 'express';
 
 @Controller('sort/auth')
 export class AuthController {
@@ -23,5 +25,15 @@ export class AuthController {
   @Post('/login')
   async login(@Body() req: LoginDto) {
     return await this.authService.login(req);
+  }
+
+  @Post('/signup')
+  async signup(@Body() req: SignupDto, @Res() res: Response) {
+    try {
+      await this.authService.signup(req);
+      return { message: 'success signup' };
+    } catch (err) {
+      return res.status(404).send({ message: err.sqlMessage });
+    }
   }
 }
