f62c07c2661f00a01ddb24d4f9b0746db71bf285
JoEunsoo <dcpg95@gmail.com>
2023-01-09 00:40:22 +0900

    REFACTOR : 회원가입, 로그인 버튼 클릭 로직

--- client/src/components/Navbar/Login/index.tsx
+++ client/src/components/Navbar/Login/index.tsx
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useModal } from '../../../hooks/modal.hook';
 import Login from '../../../views/Login';
 import Modall from '../../Modal';
 import * as S from '../style';
 
 const NavLogin = () => {
   const navigate = useNavigate();
+  const { clickHandler } = useModal();
 
   return (
     <S.LoginSection>
       <S.LoginBox>
-        <p className="log_left" onClick={() => <Modall Components={Login} />}>
+        <p className="log_left" onClick={() => clickHandler('signup')}>
           회원가입
         </p>
-        <p className="log_right" onClick={() => null}>
+        <p className="log_right" onClick={() => clickHandler('login')}>
           로그인
         </p>
       </S.LoginBox>
