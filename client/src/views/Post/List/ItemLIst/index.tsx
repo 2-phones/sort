1a94e792c47d84860a87d84e115410ebddcc389e
JoEunsoo <dcpg95@gmail.com>
2023-01-09 17:39:01 +0900

    REFACTOR : 스켈레톤 UI 추가

--- client/src/views/Post/List/ItemLIst/index.tsx
+++ client/src/views/Post/List/ItemLIst/index.tsx
@@ -3,6 +3,7 @@ import { ItemContainer, ViewsBox, ItemListBox } from './style';
 import { calculateDate } from '../../../../util/calculateDate';
 import { LodingMotion } from '../../../../components/Loding/LodingMotion';
 import Iteminfo from '../info/index';
+import PostSkeleton from '../../../../components/Loding/Skeleton/POSTS/index';
 
 const ItemList = ({ itemsData, pageMove, isLoading }) => {
   return (
@@ -15,16 +16,22 @@ const ItemList = ({ itemsData, pageMove, isLoading }) => {
           itemsData.map((dataList) => {
             const dateResult = calculateDate(dataList.createdDate);
             return (
-              <Iteminfo
-                pageMove={pageMove}
-                img_url={dataList.img_url}
-                key={dataList.post_id}
-                post_id={dataList.post_id}
-                title={dataList.title}
-                price={dataList.price}
-                region={dataList.region}
-                createdDate={dateResult}
-              />
+              <>
+                {dataList ? (
+                  <Iteminfo
+                    pageMove={pageMove}
+                    img_url={dataList.img_url}
+                    key={dataList.post_id}
+                    post_id={dataList.post_id}
+                    title={dataList.title}
+                    price={dataList.price}
+                    region={dataList.region}
+                    createdDate={dateResult}
+                  />
+                ) : (
+                  <PostSkeleton />
+                )}
+              </>
             );
           })}
       </ItemListBox>
