80e985c007f51dea80ce66a5ff128ee0933fee96
JoEunsoo <dcpg95@gmail.com>
2023-01-12 22:04:41 +0900

    REFACTOR : tsx 로 리팩토링

--- client/src/views/Post/List/ItemLIst/index.tsx
+++ client/src/views/Post/List/ItemLIst/index.tsx
@@ -2,40 +2,36 @@ import React, { useState } from 'react';
 import { ItemContainer, ViewsBox, ItemListBox } from './style';
 import { calculateDate } from '../../../../util/calculateDate';
 import { LodingMotion } from '../../../../components/Loding/LodingMotion';
-import Iteminfo from '../info/index';
+import Iteminfo from '../Info/index';
 import PostSkeleton from '../../../../components/Loding/Skeleton/POSTS/index';
 
-const ItemList = ({ itemsData, pageMove, isLoading }) => {
+const ItemList = ({ posts, dummyData }: any) => {
   return (
     <ItemContainer>
       <ViewsBox>
-        <div>{itemsData.length} views</div>
+        <div>{posts.length} views</div>
       </ViewsBox>
       <ItemListBox>
-        {itemsData &&
-          itemsData.map((dataList) => {
-            const dateResult = calculateDate(dataList.createdDate);
-            return (
-              <>
-                {dataList ? (
-                  <Iteminfo
-                    pageMove={pageMove}
-                    img_url={dataList.img_url}
-                    key={dataList.post_id}
-                    post_id={dataList.post_id}
-                    title={dataList.title}
-                    price={dataList.price}
-                    region={dataList.region}
-                    createdDate={dateResult}
-                  />
-                ) : (
-                  <PostSkeleton />
-                )}
-              </>
-            );
-          })}
+        {posts.length
+          ? posts.map((dataList) => {
+              const dateResult = calculateDate(dataList.created_at);
+              return (
+                <Iteminfo
+                  img_url={dataList.img_url}
+                  key={dataList.post_id}
+                  post_id={dataList.post_id}
+                  title={dataList.title}
+                  price={dataList.price}
+                  region={dataList.region}
+                  createdDate={dateResult}
+                  status={dataList.status}
+                />
+              );
+            })
+          : dummyData.map((li: number) => {
+              return <PostSkeleton key={li} />;
+            })}
       </ItemListBox>
-      {isLoading ? <LodingMotion /> : null}
     </ItemContainer>
   );
 };
