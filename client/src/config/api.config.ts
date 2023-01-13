dbc01d66930c6927650b5daf059c80965037ee02
JoEunsoo <dcpg95@gmail.com>
2023-01-13 22:09:31 +0900

    CHORE : api 요청 설정

--- client/src/config/api.config.ts
+++ client/src/config/api.config.ts
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
 export const authAPI = axios.create({
-  baseURL: process.env.TEST_API,
+  baseURL: process.env.AUTH_API,
+});
+
+export const PostsAPI = axios.create({
+  baseURL: process.env.POST_API,
+});
+
+export const GoogleAPI = axios.create({
+  baseURL: process.env.GOOGLE_API,
 });
