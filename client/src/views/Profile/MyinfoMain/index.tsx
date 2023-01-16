4c059681e91f8d1b65673a25ab1b3bab7fbbb01b
JoEunsoo <dcpg95@gmail.com>
2023-01-17 00:01:08 +0900

    CHORE : 마이페이지 서버요청 api 변경 적용

--- client/src/views/Profile/MyinfoMain/index.tsx
+++ client/src/views/Profile/MyinfoMain/index.tsx
@@ -5,8 +5,9 @@ import Draws from '../Withdraw/style';
 import Wishs from '../MyWish/style';
 import { postUserInfo, betaPostUserInfo } from '../../../util/userInfo';
 
-const MyinfoMain = ({ email, name }) => {
+const MyinfoMain = ({ profile }: any) => {
   const navigate = useNavigate();
+
   return (
     <Draws.CustomerContent
       style={{
@@ -16,23 +17,18 @@ const MyinfoMain = ({ email, name }) => {
       <Draws.CustomerWithdraw>
         <Mains.UserContainter>
           <Mains.UserUSer>
-            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png" />
+            <img src={profile?.profile_url} />
           </Mains.UserUSer>
           <Mains.UserInfo>
-            <Mains.UserID>{name}</Mains.UserID>
-            <Mains.UserEmail>{email}</Mains.UserEmail>
+            <Mains.UserID>{profile?.user_name}</Mains.UserID>
+            <Mains.UserEmail>{profile?.email}</Mains.UserEmail>
           </Mains.UserInfo>
-          <Mains.UserRight>
-            <Mains.ProfileButton onClick={() => navigate('/mypage/profile')}>
-              프로필 수정
-            </Mains.ProfileButton>
-          </Mains.UserRight>
         </Mains.UserContainter>
 
         <Mains.Just>
           <Mains.TitleMore>
             <Mains.Title>구매 상품</Mains.Title>
-            <Mains.More onClick={() => navigate('/mypage/buy')}>더보기 ﹥</Mains.More>
+            {/* <Mains.More onClick={() => navigate('/mypage/buy')}>더보기 ﹥</Mains.More> */}
           </Mains.TitleMore>
           <Wishs.Content style={{ marginBottom: '100px' }}>
             <Wishs.Item>
@@ -59,26 +55,13 @@ const MyinfoMain = ({ email, name }) => {
                 <Wishs.ItemPrice>12,345원</Wishs.ItemPrice>
               </Wishs.ItemNamePrice>
             </Wishs.Item>
-            <Wishs.Item>
-              <Wishs.ItemImg>
-                <Wishs.XButton></Wishs.XButton>
-                <Wishs.RealItem>
-                  <img src="http://hyunsik.me/wordpress/wp-content/uploads/2014/10/11.png" />
-                </Wishs.RealItem>
-              </Wishs.ItemImg>
-              <Wishs.ItemNamePrice>
-                <Wishs.ItemName>소격동 콘서트 티켓 팝니다</Wishs.ItemName>
-                <br></br>
-                <Wishs.ItemPrice>12,345원</Wishs.ItemPrice>
-              </Wishs.ItemNamePrice>
-            </Wishs.Item>
           </Wishs.Content>
         </Mains.Just>
 
         <Mains.Just>
           <Mains.TitleMore>
             <Mains.Title>판매 상품</Mains.Title>
-            <Mains.More onClick={() => navigate('/mypage/sell')}>더보기 ﹥</Mains.More>
+            {/* <Mains.More onClick={() => navigate('/mypage/sell')}>더보기 ﹥</Mains.More> */}
           </Mains.TitleMore>
           <Wishs.Content>
             <Wishs.Item>
@@ -89,19 +72,6 @@ const MyinfoMain = ({ email, name }) => {
               </Wishs.ItemImg>
               <Wishs.ItemNamePrice>
                 <Wishs.ItemName>아이유 콘서트 티켓 팝니다</Wishs.ItemName>
-                <br></br>
-                <Wishs.ItemPrice>12,345원</Wishs.ItemPrice>
-              </Wishs.ItemNamePrice>
-            </Wishs.Item>
-            <Wishs.Item>
-              <Wishs.ItemImg>
-                <Wishs.XButton></Wishs.XButton>
-                <Wishs.RealItem>
-                  <img src="http://hyunsik.me/wordpress/wp-content/uploads/2014/10/11.png" />
-                </Wishs.RealItem>
-              </Wishs.ItemImg>
-              <Wishs.ItemNamePrice>
-                <Wishs.ItemName>소격동 콘서트 티켓 팝니다</Wishs.ItemName>
                 <br></br>
                 <Wishs.ItemPrice>12,345원</Wishs.ItemPrice>
               </Wishs.ItemNamePrice>
