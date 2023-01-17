b3fa3c640d933d8026ffc43badc898e8e42d974c
JoEunsoo <dcpg95@gmail.com>
2023-01-17 23:50:32 +0900

    CHORE : 컨트롤러 에러핸들링 추가

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
 import { SignupDto } from 'src/dto/auth/signup.dto';
 import { TokenService } from 'src/services/token.service';
 import { AuthService } from './../services/auth.service';
-import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, Headers, Post, Res } from '@nestjs/common';
 import { LoginDto } from 'src/dto/auth/login.dto';
 import { Response } from 'express';
 
@@ -17,23 +17,34 @@ export class AuthController {
   }
 
   @Post('/token')
-  async getToken(@Body() req: any) {
-    const { access_token } = req;
-    return await this.authService.tokensReissue(access_token);
+  async getToken(@Headers() headers: any, @Res() res: Response) {
+    try {
+      const refresh_token = headers['authorization'].split(' ')[1];
+      const result = await this.tokenService.tokensReissue(refresh_token);
+      return res.status(200).send(result);
+    } catch (err) {
+      return res.status(401).send(err);
+    }
   }
 
   @Post('/login')
-  async login(@Body() req: LoginDto) {
-    return await this.authService.login(req);
+  async login(@Body() req: LoginDto, @Res() res: Response) {
+    try {
+      const result = await this.authService.login(req);
+      return res.status(200).send(result);
+    } catch (err) {
+      return res.status(404).send(err.message);
+    }
   }
 
   @Post('/signup')
   async signup(@Body() req: SignupDto, @Res() res: Response) {
     try {
+      console.log(req);
       await this.authService.signup(req);
-      return { message: 'success signup' };
-    } catch (err) {
-      return res.status(404).send({ message: err.sqlMessage });
+      return res.status(200).send('success signup');
+    } catch {
+      return res.status(401).send('This is a registered account');
     }
   }
 }
