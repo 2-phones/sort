e0faed37066b0e512e0b37f58328bb6bf6e8d7b4
JoEunsoo <dcpg95@gmail.com>
2023-01-17 23:55:43 +0900

    CHORE : post dto 수정

--- api/src/dto/posts/posts.dto.ts
+++ api/src/dto/posts/posts.dto.ts
@@ -1,13 +1,24 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class PostIdDto {
-  @IsNotEmpty()
   @IsString()
   post_id: string;
+}
+
+export class QueryStringArrayTestDto {
+  @IsString({ each: true })
+  queryData: string[];
 }
 
 export class UserIdDto {
   @IsNotEmpty()
   @IsString()
   user_id: string;
+}
+
+export class PostStatusDto {
+  @IsNotEmpty()
+  @IsString()
+  status: '판매중' | '판매완료';
 }
