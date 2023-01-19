afa6e077b2c789bacd4f01132b198dae8146cbcd
JoEunsoo <dcpg95@gmail.com>
2023-01-19 16:42:22 +0900

    FEAT : 게시물 정렬 로직

--- client/src/hooks/filter.hook.ts
+++ client/src/hooks/filter.hook.ts
@@ -1,12 +1,15 @@
 import { useAppSelector } from './redux.hook';
 
-export const useSort = (posts: any) => {
+export const useSort = (posts: []) => {
   const dropdownList = useAppSelector((state) => state.posts.dropdownList);
   const selectSort = useAppSelector((state) => state.posts.selectSort);
   const { key } = dropdownList.filter((li) => li.name === selectSort)[0];
-  const sortedPosts =
+  const dummy = [0, 1, 2, 3, 4, 5, 6, 7];
+
+  const action =
     selectSort === '정확순' || selectSort === '오래된순'
-      ? posts.sort((a: any, b: any) => (a[key] < b[key] ? -1 : 1))
-      : posts.sort((a: any, b: any) => (a[key] < b[key] ? 1 : -1));
-  return { sortedPosts };
+      ? posts || dummy.sort((a: any, b: any) => (a[key] < b[key] ? -1 : 1))
+      : posts || dummy.sort((a: any, b: any) => (a[key] < b[key] ? 1 : -1));
+  const sortHandler = posts ? action : null;
+  return { sortHandler };
 };
