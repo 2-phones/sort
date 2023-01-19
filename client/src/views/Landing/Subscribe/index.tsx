d2ed44df4f0f0512802872ce85f8ecd9995c9eab
JoEunsoo <dcpg95@gmail.com>
2023-01-19 16:48:52 +0900

    CHORE : 불필요한 코드 제거

--- client/src/views/Landing/Subscribe/index.tsx
+++ client/src/views/Landing/Subscribe/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import { Input } from '../../../components/Input/Input';
 import Lands from './style';
-import { InputBox } from '../../../components/auth/General/LoginStyle';
 
 const Subscribe = () => {
   return (
@@ -9,7 +9,7 @@ const Subscribe = () => {
         <Lands.SubFormContainer>
           <Lands.SubTitle>SORT의 소식을 보내드려요</Lands.SubTitle>
           <Lands.SubMini>저희 팀의 멤버가 되어보세요</Lands.SubMini>
-          <InputBox placeholder="이메일 입력 후 Enter를 누르세요." />
+          <Input placeholder="이메일 입력 후 Enter를 누르세요." padding="5px 10px" />
         </Lands.SubFormContainer>
       </Lands.SubForm>
     </Lands.Subscribe>
