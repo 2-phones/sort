2dec20b33769a108a92716cd97815986e7980a5d
JoEunsoo <dcpg95@gmail.com>
2023-01-19 16:54:33 +0900

    CHORE : 토큰 검사후 페이지 이동 로직 추가

--- client/src/components/Navbar/Buttons/index.tsx
+++ client/src/components/Navbar/Buttons/index.tsx
@@ -4,19 +4,22 @@ import { useNavigate } from 'react-router-dom';
 import * as S from '../style';
 import NavIcon from '../../Imgs/navbarimg/index';
 import { useModal } from '../../../hooks/modal.hook';
+import { tokenCheck } from '../../../util/tokenCheck';
 const NavButtons = () => {
   const { clickHandler } = useModal();
+  const token = tokenCheck();
+  const navigate = useNavigate();
   const [buttons, setButtons] = useState([
-    { id: 0, className: 'manIcon', src: NavIcon.user, click: 'login' },
-    { id: 1, className: 'msgIcon', src: NavIcon.message3, click: 'login' },
-    { id: 2, className: 'menuIcon', src: NavIcon.menu, click: null },
+    { id: 0, className: 'manIcon', src: NavIcon.user, path: '/mypage', click: 'login' },
+    { id: 1, className: 'msgIcon', src: NavIcon.message3, path: '', click: 'login' },
+    { id: 2, className: 'menuIcon', src: NavIcon.menu, path: '', click: null },
   ]);
   return (
     <S.ButtonSection>
       {/* {signUpModalOn ? <Modal /> : null} */}
       {buttons.map((li) => {
         return (
-          <p key={li.id} onClick={() => clickHandler(li.click)}>
+          <p key={li.id} onClick={() => (token ? navigate(li.path) : clickHandler(li.click))}>
             <img className={li.className} src={li.src} />
           </p>
         );
