1a1cf31f3d56545a0ecc333bfc64d3ec44b49ade
JoEunsoo <dcpg95@gmail.com>
2023-01-19 16:41:33 +0900

    CHORE : 토큰 자동 재발급 로직

--- client/src/hooks/auth.hook.ts
+++ client/src/hooks/auth.hook.ts
@@ -1,17 +1,17 @@
 import { useEffect, useState } from 'react';
-import { AuthData, createUser, signup, login } from '../api/auth.api';
+import { useNavigate } from 'react-router-dom';
+import { AuthData, createUser, signup, login, tokensReissue, kakaoAuthToken } from '../api/auth.api';
 import { useAppSelector } from './redux.hook';
 
-export const useOauth = (access_token: string) => {
-  const social_type = useAppSelector((state) => state.auth.social);
-  const loginORsignup = useAppSelector((state) => state.auth.loginORsignup);
+export const useOauth = (access_token: string, social_type: string) => {
   const data = { access_token, social_type };
-  window.alert(loginORsignup);
-  //   loginORsignup === 'signup' ? useSignup(data, 'social') : useLogin(data, 'social');
+  const authType = localStorage.getItem('auth');
+  authType === 'signup' ? useSignup(data, 'social') : useLogin(data, 'social');
 };
 
 export const useSignup = (data: object, type: string) => {
   const request = { data, type };
+
   useEffect(() => {
     const promiseCall = async () => {
       try {
@@ -34,15 +34,59 @@ export const useLogin = (data: object, type: string) => {
   useEffect(() => {
     const promiseCall = async () => {
       try {
-        const result: any = await login(request);
-        localStorage.getItem(result);
+        const result = await login(request);
+        const { access_token, refresh_token } = result.data;
+        localStorage.setItem('access_token', access_token);
+        localStorage.setItem('refresh_token', refresh_token);
         location.replace('/main');
       } catch (err: any) {
-        const message = err.response.data.message;
-        window.alert(message);
+        const message = err.response.data;
+        window.alert(message.message);
         location.replace('/main');
       }
     };
     promiseCall();
   }, [data]);
+};
+
+export const useTokenReissue = () => {
+  const [count, setCount] = useState(0);
+  const refresh_token = localStorage.getItem('refresh_token') || '';
+
+  useEffect(() => {
+    const promiseCall = async () => {
+      try {
+        const result = await tokensReissue(refresh_token);
+        const { access_token } = result.data || '';
+        localStorage.setItem('access_token', access_token);
+        setTimeout(() => setCount(count + 1), 890000);
+      } catch {
+        window.alert('세션 만료로 로그아웃 되었습니다.');
+        localStorage.clear();
+        location.replace('/main');
+      }
+    };
+    promiseCall();
+  }, [count]);
+};
+
+export const useKaKaoOAuth = (code: string, social_type: string) => {
+  const [access_token, setResponse] = useState('');
+  const naviagte = useNavigate();
+  useEffect(() => {
+    const promiseCall = async () => {
+      try {
+        const result = await kakaoAuthToken(code);
+        const { access_token } = result.data;
+        localStorage.setItem('kakaoToken', access_token);
+        setResponse(access_token);
+        naviagte('/oauth');
+      } catch (err: any) {
+        window.alert('카카오 정보 에러');
+      }
+    };
+    promiseCall();
+  }, []);
+
+  return { access_token };
 };
