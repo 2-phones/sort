8cf142e1bd2fb7c7fa53a64e0281e108db662fd9
JoEunsoo <dcpg95@gmail.com>
2023-01-19 16:42:53 +0900

    FEAT : 게시물 비지니스 로직

--- client/src/hooks/usePosts.hook.ts
+++ client/src/hooks/usePosts.hook.ts
@@ -4,18 +4,20 @@ import { useEffect, useState } from 'react';
 import { getPosts } from '../api/posts.api';
 import { useSort } from './filter.hook';
 
-export const usePosts = (type: string | undefined) => {
+export const useGetPosts = (type: string | undefined) => {
   const [responsePosts, setPosts] = useState<any>([]);
   const viewMore = type === 'landing' ? 5 : useAppSelector((state) => state.posts.views);
   const statusAPI = useAppSelector((state) => state.posts.statusAPI);
   const selectSort = useAppSelector((state) => state.posts.selectSort);
-  const { sortedPosts } = useSort(responsePosts);
-  const posts = sortedPosts.slice(0, viewMore);
+  const { sortHandler } = useSort(responsePosts);
+  const posts = sortHandler ? sortHandler.slice(0, viewMore) : null;
 
   useEffect(() => {
     const promiseCall = async () => {
-      const result = await getPosts(`posts/${statusAPI}`);
-      setPosts(result);
+      try {
+        const result = await getPosts(`posts/${statusAPI}`);
+        setPosts(result);
+      } catch {}
     };
     promiseCall();
   }, [viewMore, statusAPI, selectSort]);
@@ -23,7 +25,7 @@ export const usePosts = (type: string | undefined) => {
   return { posts };
 };
 
-export const usePostId = (id: string | undefined) => {
+export const useGetPostId = (id: string | undefined) => {
   const [post, setPosts] = useState<any>({});
   useEffect(() => {
     const promiseCall = async () => {
@@ -34,4 +36,8 @@ export const usePostId = (id: string | undefined) => {
   }, [id]);
 
   return { post };
+};
+
+export const useCreatePost = () => {
+  const access_token = localStorage.getItem('access_token');
 };
