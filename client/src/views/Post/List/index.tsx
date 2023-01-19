85209fd966d75353fef5a007f260b59262d4b727
JoEunsoo <dcpg95@gmail.com>
2023-01-19 16:53:32 +0900

    CHORE : 불필요한 코드 제거

--- client/src/views/Post/List/index.tsx
+++ client/src/views/Post/List/index.tsx
@@ -1,17 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { MainContainer, MainContent } from './style';
 import ItemList from './ItemLIst/index';
 import ViewMore from './ViewMore/index';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
 import FilterBar from './PostsFilter';
-import { usePosts } from '../../../hooks/usePosts.hook';
-import { PostsType } from '../../../redux/InitialState/posts.initialState';
-import { useSort } from '../../../hooks/filter.hook';
+import { useGetPosts } from '../../../hooks/usePosts.hook';
+import { useAppSelector } from '../../../hooks/redux.hook';
 
 const ItemsMain = () => {
-  const { posts } = usePosts();
-  const dummyData = [0, 1, 2, 3, 4, 5, 6, 7];
+  const { posts } = useGetPosts('');
+  const dummyData = useAppSelector((state) => state.posts.dummyData);
 
   return (
     <MainContainer>
