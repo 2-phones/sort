9fd0ab48a5ff3c92254db320386f469b3f5fa170
JoEunsoo <dcpg95@gmail.com>
2023-01-19 16:48:38 +0900

    CHORE : 게시물이 없을때 스켈레톤 컴포넌트가 렌더링, 에러방지를 위한 더미데이터 추가

--- client/src/views/Landing/ItemsView/index.tsx
+++ client/src/views/Landing/ItemsView/index.tsx
@@ -3,30 +3,36 @@ import Lands from './style';
 import { RightIcon } from '../../../components/Icons/Icons';
 import { BasicBtn } from '../../../components/Button/Button';
 import { useNavigate } from 'react-router-dom';
+import { useAppSelector } from '../../../hooks/redux.hook';
+import LandingPostSkeleton from '../../../components/Loding/Skeleton/LandingPosts';
 
-const ItemsView = ({ posts }) => {
+const ItemsView = ({ posts }: any) => {
   const navigate = useNavigate();
-
+  const dummyData = useAppSelector((state) => state.posts.dummyData);
   return (
     <Lands.ItemsView_Container>
       <div className="Previe_title">최근상품</div>
       <Lands.PreviewList>
-        {posts.map((items) => {
-          return (
-            <Lands.ItemPreview key={items.post_id}>
-              <Lands.PreviewImg>
-                <img src={items.img_url} />
-              </Lands.PreviewImg>
-              <Lands.PreviewInfo>
-                <p className="title">{items.title}</p>
-                <Lands.PreviewPrice>
-                  <p className="price">{items.price}</p>
-                  <p className="won">원</p>
-                </Lands.PreviewPrice>
-              </Lands.PreviewInfo>
-            </Lands.ItemPreview>
-          );
-        })}
+        {posts
+          ? posts.map((items) => {
+              return (
+                <Lands.ItemPreview key={items.post_id}>
+                  <Lands.PreviewImg>
+                    <img src={items.img_url} />
+                  </Lands.PreviewImg>
+                  <Lands.PreviewInfo>
+                    <p className="title">{items.title}</p>
+                    <Lands.PreviewPrice>
+                      <p className="price">{items.price}</p>
+                      <p className="won">원</p>
+                    </Lands.PreviewPrice>
+                  </Lands.PreviewInfo>
+                </Lands.ItemPreview>
+              );
+            })
+          : dummyData.slice(0, 5).map((li: number) => {
+              return <LandingPostSkeleton key={li} />;
+            })}
       </Lands.PreviewList>
       <Lands.AllviewBtn onClick={() => navigate('/main')}>
         <BasicBtn color="white" background="#363636">
