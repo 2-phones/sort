28522fb0003ad6d5fe4d20247f1937e42519d1b5
JoEunsoo <dcpg95@gmail.com>
2023-01-29 15:52:12 +0900

    CHORE : redux slice 추가

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -2,21 +2,23 @@ import { configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import itemSlice from './itemslice';
 import userSlice from './loginslice';
-import pageSlice from './MYPageSlice';
+import pageSlice from './Slices/page.slice';
 import ModalSlice from './Slices/modal.slice';
 import PostsSlice from './Slices/posts.slice';
 import AuthSlice from './Slices/auth.slice';
 import LandingSlice from './Slices/landing.slice';
+import UserSlice from './Slices/user.slice';
 
 export const store = configureStore({
   reducer: {
     items: itemSlice.reducer,
     account: userSlice.reducer,
-    mypage: pageSlice.reducer,
+    page: pageSlice.reducer,
     modal: ModalSlice.reducer,
     auth: AuthSlice.reducer,
     posts: PostsSlice.reducer,
     landing: LandingSlice.reducer,
+    user: UserSlice.reducer,
   },
 });
 
