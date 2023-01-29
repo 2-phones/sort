6852e132dcbc84feecc6ae8ddc6af89d1d40ec55
JoEunsoo <dcpg95@gmail.com>
2023-01-29 15:55:43 +0900

    FEAT : redux slice 구현

--- client/src/redux/Slices/modal.slice.ts
+++ client/src/redux/Slices/modal.slice.ts
@@ -3,12 +3,18 @@ import { useNavigate } from 'react-router-dom';
 
 interface InitialState {
   isOpen: boolean;
+  isExecute: boolean;
+  alertMsg: string;
   modalCompoents: string | null;
+  modaltypes: string;
 }
 
 const initialState: InitialState = {
   isOpen: false,
+  isExecute: false,
+  alertMsg: '',
   modalCompoents: '',
+  modaltypes: '',
 };
 
 // {id : 0 ,type : '정확순'},{id : 1 ,type : '인기순'},{id : 2 ,type : '최신순'},{id : 3 ,type : '오래된순'}
@@ -18,11 +24,20 @@ const ModalSlice = createSlice({
   initialState,
   reducers: {
     openHandler: (state, action) => {
-      state.isOpen = action.payload;
       state.modalCompoents = action.payload;
+    },
+    alertModalHandler: (state, action) => {
+      state.isOpen = action.payload ? true : false;
+      state.modaltypes = action.payload;
+    },
+    executeHandler: (state, action) => {
+      state.isExecute = action.payload;
+    },
+    addAlertMsg: (state, action) => {
+      state.alertMsg = action.payload;
     },
   },
 });
 
 export default ModalSlice;
-export const { openHandler } = ModalSlice.actions;
+export const { openHandler, alertModalHandler, executeHandler, addAlertMsg } = ModalSlice.actions;
