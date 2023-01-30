f2714292e3973756194ebd70e6804abe81fdbaef
JoEunsoo <dcpg95@gmail.com>
2023-01-30 15:57:52 +0900

    CHORE : 불필요한 코드 제거

--- client/src/views/Landing/main.tsx
+++ client/src/views/Landing/main.tsx
@@ -1,19 +1,15 @@
 import SubBanner from './SubBanner';
 import ItemsView from './ItemsView/index';
-import { getItems } from '../../util/requestItem';
 import React from 'react';
 import Lands from './mainStyle';
 import { useNavigate } from 'react-router-dom';
-import { VscGithubAlt } from 'react-icons/vsc';
-import { IoLogoInstagram } from 'react-icons/io';
 import LandingCarousel from './Carousel/index';
 import MainBanner from './MainBanner/index';
 import Subscribe from './Subscribe/index';
-import { useGetPosts } from '../../hooks/usePosts.hook';
+import { useGetPosts } from '../../hooks/post/posts.hook';
 
 const LandingMain = () => {
   const { posts } = useGetPosts('landing');
-  const navigate = useNavigate();
   return (
     <>
       <Lands.Container>
