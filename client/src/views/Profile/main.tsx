a29107d3b7e2d20df292863dd7047bac171cf7b2
JoEunsoo <dcpg95@gmail.com>
2023-01-31 16:16:56 +0900

    REFACTOR : 마이페이지 리팩토링

--- client/src/views/Profile/main.tsx
+++ client/src/views/Profile/main.tsx
@@ -5,21 +5,21 @@ import SideBar from './SideBar';
 import MyProfile from './MyProfile';
 import MyReview from './MyReview';
 import MySell from './MySellBuy/MySell';
-import MyBuy from './MySellBuy/MyBuy';
 import MyWish from './MyWish';
 import MyResion from './MyResion';
 import Withdraw from './Withdraw';
 import MyinfoMain from './MyinfoMain';
-import { useGetProfile } from '../../hooks/userProfile.hook';
+import { useGetProfile, useGetUserPosts } from '../../hooks/user/profile.hook';
+import Sellbtn from '../../components/Button/Sellbtn';
 
 const Myinfo = () => {
-  const pageNum = useSelector((state: any) => state.mypage.pageNumber);
+  const pageNum = useSelector((state: any) => state.page.pageNumber);
   const profile = useGetProfile();
+  const { posts } = useGetUserPosts();
   const pages = [
-    <MyinfoMain profile={profile} />,
-    <MyBuy />,
-    <MySell />,
-    <MyWish />,
+    <MyinfoMain profile={profile} posts={posts} />,
+    <MySell posts={posts} />,
+    <MyWish posts={[]} />,
     <MyProfile profile={profile} />,
     <MyReview />,
     <MyResion />,
@@ -27,6 +27,7 @@ const Myinfo = () => {
   ];
   return (
     <Containers>
+      <Sellbtn />
       <SideBar />
       {pages[pageNum]}
       {/* <MyReview /> */}
