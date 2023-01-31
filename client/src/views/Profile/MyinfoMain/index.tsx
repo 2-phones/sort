a0d427b1a4637dc461d4013c878a76d04378278a
JoEunsoo <dcpg95@gmail.com>
2023-01-31 16:18:57 +0900

    CHORE : 불필요한 코드제거

--- client/src/views/Profile/MyinfoMain/index.tsx
+++ client/src/views/Profile/MyinfoMain/index.tsx
@@ -1,84 +1,46 @@
 import React from 'react';
-import Mains from './style';
-import { useNavigate } from 'react-router-dom';
-import Draws from '../Withdraw/style';
-import Wishs from '../MyWish/style';
+import * as S from './style';
+import SubPostComponent from '../../../components/post/subPost';
 
-const MyinfoMain = ({ profile }: any) => {
-  const navigate = useNavigate();
-
+const MyinfoMain = ({ profile, posts }: any) => {
   return (
-    <Draws.CustomerContent
-      style={{
-        minHeight: '1020px',
-      }}
-    >
-      <Draws.CustomerWithdraw>
-        <Mains.UserContainter>
-          <Mains.UserUSer>
-            <img src={profile?.profile_url} />
-          </Mains.UserUSer>
-          <Mains.UserInfo>
-            <Mains.UserID>{profile?.user_name}</Mains.UserID>
-            <Mains.UserEmail>{profile?.email}</Mains.UserEmail>
-          </Mains.UserInfo>
-        </Mains.UserContainter>
-
-        <Mains.Just>
-          <Mains.TitleMore>
-            <Mains.Title>구매 상품</Mains.Title>
-            {/* <Mains.More onClick={() => navigate('/mypage/buy')}>더보기 ﹥</Mains.More> */}
-          </Mains.TitleMore>
-          <Wishs.Content style={{ marginBottom: '100px' }}>
-            <Wishs.Item>
-              <Wishs.ItemImg>
-                <Wishs.RealItem>
-                  <img src="https://ccimg.hellomarket.com/images/2017/item/s9/12/06/23/2514_2778861_1.jpg?size=s6" />
-                </Wishs.RealItem>
-              </Wishs.ItemImg>
-              <Wishs.ItemNamePrice>
-                <Wishs.ItemName>아이유 콘서트 티켓 팝니다</Wishs.ItemName>
-                <br></br>
-                <Wishs.ItemPrice>12,345원</Wishs.ItemPrice>
-              </Wishs.ItemNamePrice>
-            </Wishs.Item>
-            <Wishs.Item>
-              <Wishs.ItemImg>
-                <Wishs.RealItem>
-                  <img src="https://ccimg.hellomarket.com/images/2017/item/s9/12/06/23/2514_2778861_1.jpg?size=s6" />
-                </Wishs.RealItem>
-              </Wishs.ItemImg>
-              <Wishs.ItemNamePrice>
-                <Wishs.ItemName>아이유 콘서트 티켓 팝니다</Wishs.ItemName>
-                <br></br>
-                <Wishs.ItemPrice>12,345원</Wishs.ItemPrice>
-              </Wishs.ItemNamePrice>
-            </Wishs.Item>
-          </Wishs.Content>
-        </Mains.Just>
-
-        <Mains.Just>
-          <Mains.TitleMore>
-            <Mains.Title>판매 상품</Mains.Title>
-            {/* <Mains.More onClick={() => navigate('/mypage/sell')}>더보기 ﹥</Mains.More> */}
-          </Mains.TitleMore>
-          <Wishs.Content>
-            <Wishs.Item>
-              <Wishs.ItemImg>
-                <Wishs.RealItem>
-                  <img src="https://ccimg.hellomarket.com/images/2017/item/s9/12/06/23/2514_2778861_1.jpg?size=s6" />
-                </Wishs.RealItem>
-              </Wishs.ItemImg>
-              <Wishs.ItemNamePrice>
-                <Wishs.ItemName>아이유 콘서트 티켓 팝니다</Wishs.ItemName>
-                <br></br>
-                <Wishs.ItemPrice>12,345원</Wishs.ItemPrice>
-              </Wishs.ItemNamePrice>
-            </Wishs.Item>
-          </Wishs.Content>
-        </Mains.Just>
-      </Draws.CustomerWithdraw>
-    </Draws.CustomerContent>
+    <S.Container>
+      <S.UserContainter>
+        <S.UserUSer>
+          <img src={profile?.profile_url} />
+        </S.UserUSer>
+        <S.UserInfo>
+          <S.UserID>{profile?.user_name}</S.UserID>
+          <S.UserEmail>{profile?.email}</S.UserEmail>
+        </S.UserInfo>
+      </S.UserContainter>
+      <S.Posts>
+        <S.Title>
+          <p>최근 거래</p>
+        </S.Title>
+        <S.PostsBody>
+          {!posts[0] ? (
+            <S.PostsDefaultMsg>
+              <p>사용하지 않는 티켓을 등록 해보세요!</p>
+            </S.PostsDefaultMsg>
+          ) : (
+            posts.map((post: any) => {
+              return (
+                <S.PostContainer key={post.post_id}>
+                  <SubPostComponent
+                    post_id={post?.post_id}
+                    img_url={post?.img_url}
+                    status={post?.status}
+                    title={post?.title}
+                    price={post?.price}
+                  />
+                </S.PostContainer>
+              );
+            })
+          )}
+        </S.PostsBody>
+      </S.Posts>
+    </S.Container>
   );
 };
 
