474ed6e461cc9149e588345217743eaf516285c8
JoEunsoo <dcpg95@gmail.com>
2023-02-01 16:22:15 +0900

    CHORE : 마이페이지 게시물 컴포넌트 리팩토링

--- client/src/views/Profile/MySellBuy/MySell.tsx
+++ client/src/views/Profile/MySellBuy/MySell.tsx
@@ -1,77 +1,53 @@
-import React, { useState } from 'react';
-import Draws from '../Withdraw/style';
-import Sells from './SellStyle';
-import { deleteItem, getItems } from '../../../util/requestItem';
+import React from 'react';
+import * as S from './style';
 import { BasicBtn } from '../../../components/Button/Button';
-import { useDispatch, useSelector } from 'react-redux';
-import { checkItems } from '../../../redux/itemslice';
-import { useNavigate } from 'react-router-dom';
+import SubPostComponent from '../../../components/post/subPost';
 
-// 판매상품이 있을 때
-function MySell() {
-  const navigate = useNavigate();
-  const [count, setcount] = useState([]);
-  const itemList = getItems('/')[0].slice(12, 15);
-  const sellItems = [...itemList];
-  const [isCheck, setIsCheck] = useState();
-  const editItem = (value) => {
-    setIsCheck(value[2]);
-    dispatch(checkItems(value));
-  };
+import {useAppSelector } from '../../../hooks/redux.hook';
+import { useMyPost } from '../../../hooks/user/post.hook';
 
-  const { deleteData } = deleteItem();
-  const deleteitems = () => {
-    alert('삭제되었습니다.');
-    sellItems.pop(2);
-    location.reload();
-  };
-  const dispatch = useDispatch();
-  const check = useSelector((state) => state.items.itemsId);
-  checkItems;
+function MySell({ posts }: any) {
+  const postID = useAppSelector((state) => state.user.postID);
+  const { editPostHandler, deleteHandler, checkedHandler } = useMyPost();
+
   return (
-    <>
-      
-      <Draws.CustomerContent>
-        <Draws.CustomerWithdraw>
-          <Draws.ContentTitle>
-            <Draws.Title style={{ paddingBottom: '8px' }}>판매 상품</Draws.Title>
-          </Draws.ContentTitle>
-          <Sells.Content>
-            {sellItems.map((items, i) => {
+    <S.Container>
+      <h2>판매상품</h2>
+      <S.Posts>
+        <S.PostsBody>
+          {!posts[0] ? (
+            <S.PostsDefaultMsg>
+              <p>사용하지 않는 티켓을 등록 해보세요!</p>
+            </S.PostsDefaultMsg>
+          ) : (
+            posts.map((post: any) => {
               return (
-                <Sells.ItemList key={items.id}>
-                  <input
-                    name="items"
-                    value={i}
-                    onChange={(e) => editItem([e.target.checked, items.id, i])}
-                    type="checkbox"
-                    id="checkbox"
-                    checked={isCheck === i}
+                <S.PostContainer key={post.post_id}>
+                  <S.PostWrappper onClick={() => checkedHandler(post.post_id)}>
+                    <input checked={postID === post.post_id ? true : false} type="checkbox" readOnly />
+                  </S.PostWrappper>
+                  <SubPostComponent
+                    post_id={post?.post_id}
+                    img_url={post?.img_url}
+                    status={post?.status}
+                    title={post?.title}
+                    price={post?.price}
                   />
-                  <Sells.Item>
-                    <img src="http://hyunsik.me/wordpress/wp-content/uploads/2014/10/11.png" />
-                    <Sells.ItemDetail>
-                      <Sells.ItemName>{items.title}</Sells.ItemName>
-                      <Sells.ItemSeller>페샷추존맛</Sells.ItemSeller>
-                      <Sells.ItemPrice>{items.price}</Sells.ItemPrice>
-                    </Sells.ItemDetail>
-                  </Sells.Item>
-                </Sells.ItemList>
+                </S.PostContainer>
               );
-            })}
-            <Sells.BtnSection>
-              <Sells.AcitonBtn onClick={() => navigate(`/mypage/sell/edit/${check}`)}>
-                <BasicBtn background="black">상품 수정</BasicBtn>
-              </Sells.AcitonBtn>
-              <Sells.AcitonBtn onClick={() => deleteData(check)}>
-                <BasicBtn background="black">상품 삭제</BasicBtn>
-              </Sells.AcitonBtn>
-            </Sells.BtnSection>
-            {/* <img src="https://ccimg.hellomarket.com/images/2017/item/s9/12/06/23/2514_2778861_1.jpg?size=s6" /> */}
-          </Sells.Content>
-        </Draws.CustomerWithdraw>
-      </Draws.CustomerContent>
-    </>
+            })
+          )}
+        </S.PostsBody>
+      </S.Posts>
+      <S.BtnSection>
+        <S.AcitonBtn onClick={() => editPostHandler(postID)}>
+          <BasicBtn background="black">상품 수정</BasicBtn>
+        </S.AcitonBtn>
+        <S.AcitonBtn onClick={() => deleteHandler()}>
+          <BasicBtn background="black">상품 삭제</BasicBtn>
+        </S.AcitonBtn>
+      </S.BtnSection>
+    </S.Container>
   );
 }
 
