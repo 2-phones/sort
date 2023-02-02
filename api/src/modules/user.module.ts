a6ca8edc5d05f6a9db9d400dfad8041f64bc7d25
JoEunsoo <dcpg95@gmail.com>
2023-02-03 08:02:51 +0900

    CHORE : 게시물 모듈 추가

--- api/src/modules/user.module.ts
+++ api/src/modules/user.module.ts
@@ -5,9 +5,10 @@ import { UserController } from './../controllers/user.controller';
 import { Module } from '@nestjs/common';
 import { UserService } from 'src/services/user.service';
 import { UsersEntity } from 'src/entities/users.entity';
+import { PostModule } from './post.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsersEntity]), TokenModule],
+  imports: [TypeOrmModule.forFeature([UsersEntity]), TokenModule, PostModule],
   providers: [UserService, UsersRepository],
   controllers: [UserController],
 })
