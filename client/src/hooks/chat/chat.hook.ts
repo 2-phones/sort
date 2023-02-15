ebcc2a6560338c624dc88f4b42725b2835fc9cba
JoEunsoo <dcpg95@gmail.com>
2023-02-15 23:23:02 +0900

    FEAT : 채팅 컴포넌트 비즈니스 로직 구현

---
+++ client/src/hooks/chat/chat.hook.ts
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+import { socket } from '../../config/chat.config';
+
+export const useChat = () => {
+  const [room, setRoom] = useState<any>('');
+
+  const chatHandle = async () => {
+    try {
+      const result = socket.emit('message', '');
+      setRoom(result);
+      console.log(result);
+    } catch (err: any) {
+      console.log(err);
+    }
+  };
+  chatHandle();
+};
