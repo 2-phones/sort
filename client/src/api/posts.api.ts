fd5955b2f96e581e8861e71d8784a31047e9b4ae
JoEunsoo <dcpg95@gmail.com>
2023-02-25 15:30:12 +0900

    FEAT : post 수정,삭제 api 로직 구현

--- client/src/api/posts.api.ts
+++ client/src/api/posts.api.ts
@@ -9,14 +9,40 @@ export const createPost = async (data: any) => {
     return;
   }
 
-  await PostsAPI.post('posts', data, {
+  return await PostsAPI.post('posts', data, {
     headers: {
       Authorization: `Bearer ${access_token}`,
     },
   });
 };
-const editPosts = async () => {};
-const deltePosts = async () => {};
+
+export const editPost = async (data: any, post_id: string | undefined) => {
+  const access_token = localStorage.getItem('access_token');
+
+  if (!access_token) {
+    return;
+  }
+
+  return await PostsAPI.put(`posts/${post_id}`, data, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+};
+
+export const deletePost = async (post_id: string) => {
+  const access_token = localStorage.getItem('access_token');
+
+  if (!access_token) {
+    return;
+  }
+
+  return await PostsAPI.delete(`posts/${post_id}`, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+};
 
 export const getPosts = async (api: string) => {
   try {
