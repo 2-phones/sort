34191fe7d0d6d448c9458c540c553dc57690dbb7
JoEunsoo <dcpg95@gmail.com>
2023-03-03 15:21:17 +0900

    CHORE : 배포 환경에 따른 CORS 설정

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,8 +6,13 @@ import { AppModule } from './modules/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(Helmet());
+
+  // 배포환경에 따라 cors 설정
   app.enableCors({
-    origin: true,
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? [process.env.CLIENT_URL, process.env.IP_URL]
+        : true,
     methods: ['GET', 'HEAD', 'PUT', 'POST', 'DELETE'],
     credentials: true,
   });
