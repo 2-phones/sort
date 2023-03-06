799828ea62a5e613acf9a0174ed59f29d40ffb53
JoEunsoo <dcpg95@gmail.com>
2023-03-06 16:23:28 +0900

    FEAT : 채팅 컨트롤러 구현

--- api/src/controllers/chat.controller.ts
+++ api/src/controllers/chat.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { EventsGateway } from './../gateway/chat.gateway';
+import { ChatService } from './../services/chat.service';
+import { Body, Controller, Headers, Logger, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { PostIdDto } from 'src/dto/posts/posts.dto';
+import { TokenService } from 'src/services/token.service';
 
 @Controller('sort/chat')
-export class ChatController {}
+export class ChatController {
+  private readonly logger = new Logger(ChatController.name);
+
+  constructor(
+    private readonly tokenService: TokenService,
+    private readonly chatService: ChatService,
+    private readonly enventsGateway: EventsGateway,
+  ) {}
+
+  @Post()
+  async create(
+    @Headers() headers: any,
+    @Body() chatMessage: any,
+    @Res() res: Response,
+  ) {
+    try {
+      this.logger.log(chatMessage);
+      const access_token = headers['authorization'].split(' ')[1];
+      const { user_id } = await this.tokenService.decodeToken(access_token);
+      const chatData = { user_id, ...chatMessage };
+      await this.chatService.createChatRoom(chatData);
+      return res.status(200).send(chatMessage);
+    } catch (err) {
+      return res.status(404).send(err);
+    }
+  }
+}
