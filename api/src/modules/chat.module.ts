2785caf46745798ee5830953816e456c1d8eef06
JoEunsoo <dcpg95@gmail.com>
2023-03-06 16:19:37 +0900

    CHORE : envents-gateway 모듈 추가

--- api/src/modules/chat.module.ts
+++ api/src/modules/chat.module.ts
@@ -1,8 +1,13 @@
+import { EventsModule } from './events.module';
+import { ChatService } from './../services/chat.service';
+import { ChatController } from './../controllers/chat.controller';
 import { Module } from '@nestjs/common';
 import { EventsGateway } from '../gateway/chat.gateway';
+import { TokenModule } from './token.module';
 
 @Module({
-  providers: [EventsGateway],
-  exports: [EventsGateway],
+  imports: [EventsModule, TokenModule],
+  providers: [ChatService],
+  controllers: [ChatController],
 })
 export class ChatModule {}
