37884add98e47fbe47938275221e56f7dde21668
JoEunsoo <dcpg95@gmail.com>
2023-03-06 16:23:39 +0900

    FEAT : 채팅 비즈니스 로직 구현

--- api/src/services/chat.service.ts
+++ api/src/services/chat.service.ts
@@ -1,4 +1,11 @@
+import { EventsGateway } from './../gateway/chat.gateway';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+  constructor(private readonly eventsGateway: EventsGateway) {}
+
+  async createChatRoom(chatData: any) {
+    return this.eventsGateway.handleMessage(chatData);
+  }
+}
