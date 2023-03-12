d2a39e512c3173f95b302dd252c7ffed04464fc6
EunSooJ <87120463+2-phones@users.noreply.github.com>
2023-03-12 23:09:22 +0900

    Delete frontend directory

--- frontend/client/src/util/tokenCheck.ts
+++
@@ -1,8 +0,0 @@
-export const tokenCheck = (): boolean => {
-  return localStorage.getItem('access_token') ? true : false;
-};
-
-export const tokendelete = (): void => {
-  localStorage.clear();
-  location.replace('/main');
-};
