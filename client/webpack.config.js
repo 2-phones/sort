e81717b16d743fb6a2d650d34740b00a8c5c8ada
JoEunsoo <dcpg95@gmail.com>
2023-05-04 01:20:49 +0900

    CHORE : import react 생략을 위해 tsconfig jsxImportSource 옵션 추가

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -30,6 +30,9 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'), 
+    },
   },
   stats: 'errors-only',
   devServer: {
