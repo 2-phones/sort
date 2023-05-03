b6a97141f60a8b4390a0003b2a5380c1df2545af
JoEunsoo <dcpg95@gmail.com>
2023-05-03 15:15:32 +0900

    FEAT : 게시물 판매 유형별 조건부 렌더링 추가

--- frontend/client/src/views/Post/List/ItemLIst/index.tsx
+++ frontend/client/src/views/Post/List/ItemLIst/index.tsx
@@ -3,8 +3,10 @@ import { ItemContainer, ViewsBox, ItemListBox, ItemWrapper } from './style';
 import { calculateDate } from '../../../../util/calculateDate';
 import Iteminfo from '../Info/index';
 import PostSkeleton from '../../../../components/Loding/Skeleton/POSTS/index';
+import { useAppSelector } from '../../../../hooks/common/redux.hook';
 
 const ItemList = ({ posts, dummyData }: any) => {
+  const { sellType } = useAppSelector((state) => state.posts);
   return (
     <ItemContainer>
       <ViewsBox>
@@ -12,22 +14,24 @@ const ItemList = ({ posts, dummyData }: any) => {
       </ViewsBox>
       <ItemListBox>
         {posts.length
-          ? posts.map((dataList) => {
-              const dateResult = calculateDate(dataList.created_at);
-              return (
-                <ItemWrapper key={dataList.post_id}>
-                  <Iteminfo
-                    img_url={dataList.img_url}
-                    post_id={dataList.post_id}
-                    title={dataList.title}
-                    price={dataList.price}
-                    region={dataList.region}
-                    createdDate={dateResult}
-                    status={dataList.status}
-                  />
-                </ItemWrapper>
-              );
-            })
+          ? posts
+              .filter((dataList) => (sellType === '전체' ? true : dataList.status === sellType))
+              .map((dataList) => {
+                const dateResult = calculateDate(dataList.created_at);
+                return (
+                  <ItemWrapper key={dataList.post_id}>
+                    <Iteminfo
+                      img_url={dataList.img_url}
+                      post_id={dataList.post_id}
+                      title={dataList.title}
+                      price={dataList.price}
+                      region={dataList.region}
+                      createdDate={dateResult}
+                      status={dataList.status}
+                    />
+                  </ItemWrapper>
+                );
+              })
           : dummyData.map((li: number) => {
               return <PostSkeleton key={li} />;
             })}
