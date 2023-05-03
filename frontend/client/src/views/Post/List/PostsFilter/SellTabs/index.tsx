e11d62734f07f9f344d42647bb9745520c2c8412
JoEunsoo <dcpg95@gmail.com>
2023-05-03 15:16:09 +0900

    REFACTOR : Redux 판매유형 상태관리 수정으로 인한 코드 변경

--- frontend/client/src/views/Post/List/PostsFilter/SellTabs/index.tsx
+++ frontend/client/src/views/Post/List/PostsFilter/SellTabs/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useAppDispatch, useAppSelector } from '../../../../../hooks/common/redux.hook';
-import { isFocues } from '../../../../../redux/Slices/posts.slice';
+import { isFocues, sellStatusHandle } from '../../../../../redux/Slices/posts.slice';
 import { SellType } from './style';
 
 const SellTabs = () => {
   const dispatch = useAppDispatch();
   const selltabs = useAppSelector((state) => state.posts.selltabs);
-  const isfocus = useAppSelector((state) => state.posts.focusTab);
+  const { sellType } = useAppSelector((state) => state.posts);
 
   return (
     <>
@@ -14,8 +14,8 @@ const SellTabs = () => {
         return (
           <SellType
             key={tab.id}
-            color={isfocus === tab.name ? 'black' : ''}
-            onClick={() => dispatch(isFocues(tab.name))}
+            color={sellType === tab.name ? 'black' : ''}
+            onClick={() => dispatch(sellStatusHandle(tab.name))}
           >
             <p>{tab.name}</p>
           </SellType>
