7b1d2c0742603366e2e4ce6d31381a5b35d31631
JoEunsoo <dcpg95@gmail.com>
2023-05-04 01:35:36 +0900

    REMOVE : client 작업으로 불필요한 파일 제거

--- backend/api/src/entities/common.entity.ts
+++
@@ -1,9 +0,0 @@
-import { CreateDateColumn, PrimaryColumn } from 'typeorm';
-
-export class CommonEntity {
-  @PrimaryColumn()
-  user_id: string;
-
-  @CreateDateColumn()
-  created_at: Date;
-}
--- backend/api/src/util/randomString.util.ts
+++
@@ -1,3 +0,0 @@
-export const createid = () => {
-  return Math.floor(Math.random() * Date.now()).toString(16);
-};
