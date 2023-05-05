475f7d759691a9f60233296be7925a95f4796bec
JoEunsoo <dcpg95@gmail.com>
2023-05-06 00:14:37 +0900

    CHORE: 이미지import,export 관리를 위한 ts파일

---
+++ frontend/client/src/assets/imgs/image.ts
@@ -0,0 +1,7 @@
+import chat from './chat.svg';
+import handshake from './handshake.svg';
+import mokup from './mokup.png';
+import region from './region.svg';
+import search from './search.svg';
+
+export { chat, handshake, mokup, region, search };
