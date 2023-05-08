59a3a02db4b61dad1ec544b3ac84c4f58084d856
JoEunsoo <dcpg95@gmail.com>
2023-05-08 10:35:12 +0900

    FEAT : 페이지 react-router 추가

--- frontend/client/src/app.tsx
+++ frontend/client/src/app.tsx
@@ -1,15 +1,23 @@
 import React, { useEffect } from 'react';
 import GlobalStyle from '@/styles/global.style';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { motion } from 'framer-motion';
+import { appRoutes } from './routes/app.routes';
+import { store } from './redux/store';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <GlobalStyle />
-
-      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}></motion.div>
+      <Provider store={store}>
+        <GlobalStyle />
+        {/* <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}></motion.div> */}
+        <Routes>
+          {appRoutes.map((routeList, index) => (
+            <Route key={index} path={routeList.path} element={routeList.element} />
+          ))}
+        </Routes>
+      </Provider>
     </BrowserRouter>
   );
 };
