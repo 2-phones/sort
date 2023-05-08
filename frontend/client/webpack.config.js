cfa0dbbccec21218bd8f18c4249adfc7879caba7
JoEunsoo <dcpg95@gmail.com>
2023-05-08 10:34:31 +0900

    CHORE : webpack import 경로 생략 옵션, smp 옵션, esbuild-loader 추가

--- frontend/client/webpack.config.js
+++ frontend/client/webpack.config.js
@@ -3,11 +3,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-const refreshPlugin = ['react-refresh/babel'];
 const mode = process.env.NODE_ENV || 'development';
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const dotenv = require('dotenv').config({
   path: process.env.NODE_ENV == 'production' ? './.env.production' : './.env.development',
 });
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+const smp = new SpeedMeasurePlugin();
+const analyzer = process.env.BUNDLE_ANALYZER === 'true' ? new BundleAnalyzerPlugin() : null;
 
 module.exports = {
   mode,
@@ -15,30 +18,49 @@ module.exports = {
   devtool: 'eval',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
+    filename: '[name].bundle.js',
     publicPath: '/',
+    // clean: true,
+  },
+  // profile: true,
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'), 
+    },
   },
   stats: 'errors-only',
   devServer: {
     hot: true,
     compress: true,
-    port: 3001,
+    static: { directory: path.resolve(__dirname, 'dist') },
+    port: 3002,
     historyApiFallback: true,
   },
   module: {
     rules: [
       {
+        // test: /\.(ts|tsx)$/,
+        // exclude: /node_modules/,
+        // loader: 'babel-loader',
+        // options: {
+        //   presets: ['@babel/preset-typescript', '@babel/preset-env', '@babel/preset-react'],
+        //   plugins: mode === 'development' ? refreshPlugin : null,
+        // },
+
         test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: 'esbuild-loader',
         options: {
-          presets: ['@babel/preset-typescript', '@babel/preset-env', '@babel/preset-react'],
-          plugins: mode === 'development' ? refreshPlugin : null,
+          loader: 'tsx',
+          target: 'es2015',
         },
       },
+
       {
         test: /\.(png|jpg)$/,
         use: ['file-loader'],
@@ -57,5 +79,6 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({ template: './public/index.html', favicon: './public/logo.ico' }),
     new RefreshWebpackPlugin(),
-  ],
+    analyzer,
+  ].filter(Boolean),
 };
