60456c74a3dded406c1d4a352ba992c4ce15668e
JoEunsoo <dcpg95@gmail.com>
2023-05-09 23:56:33 +0900

    FEAT : Footer 컴포넌트 구현

--- frontend/client/src/components/Footer/footer.component.tsx
+++ frontend/client/src/components/Footer/footer.component.tsx
@@ -1,5 +1,35 @@
+import { useAppSelector } from '@/redux/store';
+import * as S from '@/styles/footer/footer.style';
+
 const Footer = () => {
-  return <></>;
+  const { footer_nav, footer_description } = useAppSelector((state) => state.common);
+  return (
+    <S.FooterLayOut>
+      <S.FooterBox>
+        {footer_nav.map((nav) => {
+          return (
+            <S.FooterNav key={nav.id}>
+              <h4>{nav.title}</h4>
+              <ul>
+                <li>
+                  <a href=''>{nav.name}</a>
+                </li>
+              </ul>
+            </S.FooterNav>
+          );
+        })}
+      </S.FooterBox>
+      <S.FooterBottomBox>
+        <S.FooterDescriptionLeft>
+          <p>{footer_description}</p>
+        </S.FooterDescriptionLeft>
+        <S.FooterDescriptionRight>
+          <p className="madein">South Korea</p>
+          <p>© 2022 SORT</p>
+        </S.FooterDescriptionRight>
+      </S.FooterBottomBox>
+    </S.FooterLayOut>
+  );
 };
 
 export default Footer;
