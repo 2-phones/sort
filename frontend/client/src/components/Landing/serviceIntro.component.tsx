e0cad81f159aaf5f3dd8e4e2ed6766210f2223ef
JoEunsoo <dcpg95@gmail.com>
2023-05-09 23:57:16 +0900

    FEAT : 랜딩페이지의 첫 부분 ServiceIntro 컴포넌트 구현

--- frontend/client/src/components/Landing/serviceIntro.component.tsx
+++ frontend/client/src/components/Landing/serviceIntro.component.tsx
@@ -1,8 +1,25 @@
+import { mokup } from '@/assets/imgs/image';
+import * as S from '@/styles/landingPage/serviceIntro.style';
+
 const ServiceIntro = () => {
   return (
-    <section>
-      <h2>SELL OWN REGION TICKET</h2>
-    </section>
+    <S.ServiceIntroLayOut>
+      <S.IntroBox>
+        <S.IntroTextSection>
+          <S.Title>
+            <h2>SELL OWN REGION TICKET</h2>
+          </S.Title>
+          <S.SubTitle>
+            <p className="service_intro">올바른 티켓의 시작</p>
+            <p className="description">지역인증 기반 티켓 중고 거래</p>
+          </S.SubTitle>
+          <S.Button>거래하러 가기</S.Button>
+        </S.IntroTextSection>
+        <S.IntroImageSection>
+          <img src={mokup} />
+        </S.IntroImageSection>
+      </S.IntroBox>
+    </S.ServiceIntroLayOut>
   );
 };
 
