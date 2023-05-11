41591da04ebcdaaa7973104d8721f6b450329887
JoEunsoo <dcpg95@gmail.com>
2023-05-11 10:14:23 +0900

    FEAT : 랜딩 페이지를 제외한 모든 페이지에 Header 컴포넌트 추가

--- frontend/client/src/app.tsx
+++ frontend/client/src/app.tsx
@@ -5,6 +5,8 @@ import { motion } from 'framer-motion';
 import { appRoutes } from './routes/app.routes';
 import { store } from './redux/store';
 import Footer from './components/Footer/footer.component';
+import { Fragment } from 'react';
+import Header from './components/Header/header.component';
 
 const App = () => {
   return (
@@ -14,7 +16,16 @@ const App = () => {
         {/* <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}></motion.div> */}
         <Routes>
           {appRoutes.map((routeList, index) => (
-            <Route key={index} path={routeList.path} element={routeList.element} />
+            <Route
+              key={index}
+              path={routeList.path}
+              element={
+                <Fragment>
+                  {routeList.element.type.name !== 'LandingPage' ? <Header /> : null}
+                  {routeList.element}
+                </Fragment>
+              }
+            />
           ))}
         </Routes>
         <Footer />
