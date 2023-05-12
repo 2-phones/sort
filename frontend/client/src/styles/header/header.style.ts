a76bc1273632d06ddce8313ab99845decab5470a
JoEunsoo <dcpg95@gmail.com>
2023-05-12 10:22:22 +0900

    FEAT : Header 컴포넌트 style 구현

---
+++ frontend/client/src/styles/header/header.style.ts
@@ -0,0 +1,10 @@
+import { styled } from '@/styles/global.style';
+
+const HeaderLayOut = styled.header`
+  width: 100%;
+  border: 2px solid;
+  height: 180px;
+  position: relative;
+`;
+
+export { HeaderLayOut};
