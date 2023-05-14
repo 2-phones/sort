ab07616e911eb12f381ac94495b74c63a0734d00
JoEunsoo <dcpg95@gmail.com>
2023-05-14 05:54:27 +0430

    FEAT : Header 내부의 TopNav 컴포넌트 구현

--- frontend/client/src/components/Header/topNav.component.tsx
+++ frontend/client/src/components/Header/topNav.component.tsx
@@ -1,5 +1,15 @@
+import * as S from '@/styles/header/topNav.style';
+
 const TopNav = () => {
-  return <section></section>;
+  return (
+    <S.TopNavLayOut>
+      <S.AuthNavigation>
+        <p>회원가입</p>
+        <p className="blank_center"></p>
+        <p>로그인</p>
+      </S.AuthNavigation>
+    </S.TopNavLayOut>
+  );
 };
 
 export default TopNav;
