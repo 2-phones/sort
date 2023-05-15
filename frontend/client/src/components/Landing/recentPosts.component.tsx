0229dd898553a9c251c50d8f86968abf4f37a387
JoEunsoo <dcpg95@gmail.com>
2023-05-14 20:34:01 -0500

    FEAT : 랜딩페이지 클릭시 main 경로로 이동하는 로직 추가

--- frontend/client/src/components/Landing/recentPosts.component.tsx
+++ frontend/client/src/components/Landing/recentPosts.component.tsx
@@ -1,6 +1,8 @@
 import * as S from '@/styles/landingPage/recentPosts.style';
+import { useNavigate } from 'react-router-dom';
 const RecentPosts = () => {
   const testList = [1, 2, 3, 4, 5];
+  const navigate = useNavigate();
   return (
     <S.RecentPostLayOut>
       <h3>최근상품</h3>
@@ -15,7 +17,7 @@ const RecentPosts = () => {
         })}
       </S.RecentPostList>
       <S.ButtonSection>
-        <S.ViewAllButton>전체보기</S.ViewAllButton>
+        <S.ViewAllButton onClick={() => navigate('/main')}>전체보기</S.ViewAllButton>
       </S.ButtonSection>
     </S.RecentPostLayOut>
   );
