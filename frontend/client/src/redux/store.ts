3e4d31915c852dcc264089080058a92a04045ae9
JoEunsoo <dcpg95@gmail.com>
2023-05-21 18:03:35 +0900

    ADD : 스토어에 PostSlice 추가

--- frontend/client/src/redux/store.ts
+++ frontend/client/src/redux/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import CommonSlice from './slices/common.slice';
 import LandingSlice from './slices/landing.slice';
+import PostSlice from './slices/post.slice';
 
 const store = configureStore({
   reducer: {
     landing: LandingSlice.reducer,
     common: CommonSlice.reducer,
+    post: PostSlice.reducer,
   },
 });
 
