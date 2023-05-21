6674f97f6c0b4a9c0ca140f93b93a60d241cca75
JoEunsoo <dcpg95@gmail.com>
2023-05-21 18:00:19 +0900

    'FEAT : 메인페이지 컴포넌트 구현

--- frontend/client/src/pages/main.page.tsx
+++ frontend/client/src/pages/main.page.tsx
@@ -1,5 +1,12 @@
+import Header from '@/components/Header/header.component';
+import PostFilter from '@/components/MainPost/postFilter.component';
+
 const MainPage = () => {
-  return <div>sds</div>;
+  return (
+    <div>
+      <PostFilter />
+    </div>
+  );
 };
 
 export default MainPage;
