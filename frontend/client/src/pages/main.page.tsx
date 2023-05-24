012e002b27c5d365cb575a5d0cc65bb13eb48a77
JoEunsoo <dcpg95@gmail.com>
2023-05-25 08:00:45 +0900

    ADD : 메인페이지에 게시물 리스트 컴포넌트 추가

--- frontend/client/src/pages/main.page.tsx
+++ frontend/client/src/pages/main.page.tsx
@@ -1,10 +1,12 @@
 import Header from '@/components/Header/header.component';
 import PostFilter from '@/components/MainPost/postFilter.component';
+import PostList from '@/components/MainPost/postList.component';
 
 const MainPage = () => {
   return (
     <div>
       <PostFilter />
+      <PostList />
     </div>
   );
 };
